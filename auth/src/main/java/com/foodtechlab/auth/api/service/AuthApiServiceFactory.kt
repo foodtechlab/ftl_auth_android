78111574a781cd0615953836398b084c548a57b1
Umalt <isakkhazhiev@gmail.com>
2021-01-23 01:04:56 +0300

    Реализована передача httpLoggingInterceptor извне

--- auth/src/main/java/com/foodtechlab/auth/api/service/AuthApiServiceFactory.kt
+++ auth/src/main/java/com/foodtechlab/auth/api/service/AuthApiServiceFactory.kt
@@ -1,9 +1,7 @@
 package com.foodtechlab.auth.api.service
 
-import com.foodtechlab.auth.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
-import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 import java.util.concurrent.TimeUnit
@@ -15,32 +13,28 @@ object AuthApiServiceFactory {
 
     fun makeAuthApiService(
         baseUrl: String,
-        apiInterceptor: Interceptor?
+        apiInterceptor: Interceptor?,
+        httpLoggingInterceptor: Interceptor?
     ): AuthApiService {
         return Retrofit.Builder()
             .baseUrl(baseUrl)
-            .client(makeOkHttpClient(apiInterceptor))
+            .client(makeOkHttpClient(apiInterceptor, httpLoggingInterceptor))
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
             .create(AuthApiService::class.java)
     }
 
-    private fun makeOkHttpClient(apiInterceptor: Interceptor?): OkHttpClient {
+    private fun makeOkHttpClient(
+        apiInterceptor: Interceptor?,
+        httpLoggingInterceptor: Interceptor?
+    ): OkHttpClient {
         return OkHttpClient.Builder()
             .connectTimeout(5, TimeUnit.SECONDS)
             .readTimeout(10, TimeUnit.SECONDS)
-            .addInterceptor(makeHttpLoggingInterceptor())
-            .apply { apiInterceptor?.let { addInterceptor(it) } }
-            .build()
-    }
-
-    private fun makeHttpLoggingInterceptor(): Interceptor {
-        return HttpLoggingInterceptor().apply {
-            level = if (BuildConfig.DEBUG) {
-                HttpLoggingInterceptor.Level.BODY
-            } else {
-                HttpLoggingInterceptor.Level.NONE
+            .apply {
+                apiInterceptor?.let { addInterceptor(it) }
+                httpLoggingInterceptor?.let { addInterceptor(it) }
             }
-        }
+            .build()
     }
 }
